b230b03bb23d1aa3426b72f4c498f1856b8c7fa0
Phantasmic Dev <74925183+PhantasmicDev@users.noreply.github.com>
2023-02-27 19:24:22 -0800

    renamed INPUT_TARGET_BRANCH to INPUT_BRANCH

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
-if git ls-remote --heads origin $INPUT_TARGET_BRANCH | grep -q $INPUT_TARGET_BRANCH; then
-	git fetch origin $INPUT_TARGET_BRANCH
+if git ls-remote --heads origin $INPUT_BRANCH | grep -q $INPUT_BRANCH; then
+	git fetch origin $INPUT_BRANCH
 fi
 
-git subtree split -P $INPUT_DIRECTORY -b $INPUT_TARGET_BRANCH
-git checkout $INPUT_TARGET_BRANCH
+git subtree split -P $INPUT_DIRECTORY -b $INPUT_BRANCH
+git checkout $INPUT_BRANCH
 
 if [ -n $INPUT_TAG ]; then
 	git tag $INPUT_TAG
 fi
 
-git push -f origin $INPUT_TARGET_BRANCH
+git push -f origin $INPUT_BRANCH
 
 if [ -n $INPUT_TAG ]; then
 	git push origin $INPUT_TAG
