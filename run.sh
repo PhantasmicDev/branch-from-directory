bfdaa467e63c9124e30d260a7bff33791d4845b9
Phantasmic Dev <74925183+PhantasmicDev@users.noreply.github.com>
2023-02-27 20:24:18 -0800

    Checks out branch if desired

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
+CHECKOUT=$(echo "$INPUT_CHECKOUT" | tr '[:upper:]' '[:lower:]')
+
+if [[ "$CHECKOUT" == "true" ]]; then
+	git checkout $(basename $INPUT_REF)
+else if [[ "$CHECKOUT" != "false" ]]
+	echo "ERROR: Invalid input value for 'checkout': $INPUT_CHECKOUT"
+	exit 1
+fi
+
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
-if git ls-remote --heads origin $INPUT_BRANCH | grep -q $INPUT_BRANCH; then
-	git fetch origin $INPUT_BRANCH
-fi
+#if git ls-remote --heads origin $INPUT_BRANCH | grep -q $INPUT_BRANCH; then
+#	git fetch origin $INPUT_BRANCH
+#fi
 
 git subtree split -P $INPUT_DIRECTORY -b $INPUT_BRANCH
 git checkout $INPUT_BRANCH
